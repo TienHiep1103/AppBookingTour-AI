7249161fa8e3cd830501b00175cb9e5c772c5ffc
hiepnt <tienhiep345@gmail.com>
2025-12-25 14:08:37 +0700

    hehe2

--- app/db.py
+++ app/db.py
@@ -2,11 +2,16 @@ from app.config import CONNECTION_STRING
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+raw_conn_str = os.getenv("SQL_CONNECTION_STRING")
+
+params = urllib.parse.quote_plus(raw_conn_str)
+
 engine = create_engine(
-    CONNECTION_STRING,
+    f"mssql+pyodbc:///?odbc_connect={params}",
     pool_size=10,
     max_overflow=20,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    pool_recycle=1800,
 )
 
 SessionLocal = sessionmaker(
